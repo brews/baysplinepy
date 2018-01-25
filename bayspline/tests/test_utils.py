1daaa7bded26585ee22764d14e8cabccd89c2bc5
Brewster Malevich <sbmalev@gmail.com>
2018-01-24 19:15:44 -0700

    Fix bad spline extrapolation in predict_sst()

--- bayspline/tests/test_utils.py
+++ bayspline/tests/test_utils.py
@@ -1,9 +1,86 @@
 import pytest
 import numpy as np
+import scipy.interpolate as interpolate
 import bayspline.utils as utils
 
 
+@pytest.fixture(scope='session')
+def ppolyspline1():
+    x = np.array([-0.4,  23.6,  29.6])
+    c = np.array([[4.60901815e-05, -2.54546446e-03],
+                  [3.22603382e-02, 3.44726669e-02],
+                  [5.32351561e-02, 8.54031217e-01]])
+    return interpolate.PPoly(x=x, c=c)
+
+
+@pytest.fixture(scope='session')
+def tck1():
+    # Knots, coefficients, polynomial degree:
+    tck = [np.array([-0.4, -0.4, -0.4, 23.6, 29.6, 29.6, 29.6]),
+           np.array([0.05323516, 0.44035921, 0.95744922, 0.9692305]),
+           2]
+    return tck
+
+
 def test_augknt():
+    """Test basic utils.augknt()
+    """
     goal = np.array([1, 1, 1, 2, 2, 2])
     victim = utils.augknt([1, 2], 2)
     np.testing.assert_equal(goal, victim)
+
+
+def test_bspline2ppoly(tck1):
+    """Test basic utils.bspline2poly()
+    """
+    goal_x = np.array([-0.4,  23.6,  29.6])
+    goal_c = np.array([[4.60901815e-05, -2.54546446e-03],
+                       [3.22603382e-02, 3.44726669e-02],
+                       [5.32351561e-02, 8.54031217e-01]])
+    victim = utils.bspline2ppoly(tck1)
+    np.testing.assert_allclose(victim.x, goal_x, atol=1e-4)
+    np.testing.assert_allclose(victim.c, goal_c, atol=1e-4)
+
+
+def test_add_ppolyknot1(ppolyspline1):
+    goal_x = np.array([-1.4, -0.4, 23.6, 29.6, 30.6])
+    goal_c = np.array([[4.60901815e-05, 4.60901815e-05,
+                        -2.54546446e-03, -2.54546446e-03],
+                       [3.21681578e-02, 3.22603382e-02,
+                        3.44726669e-02, 3.92709337e-03],
+                       [2.10209081e-02, 5.32351561e-02,
+                        8.54031217e-01, 9.69230498e-01]])
+    victim = utils.add_ppolyknot(ppolyspline1, knots=[-1.4,  30.6])
+    np.testing.assert_allclose(victim.x, goal_x, atol=1e-3)
+    np.testing.assert_allclose(victim.c, goal_c, atol=1e-3)
+
+
+def test_add_ppolyknot2():
+    goal = interpolate.PPoly(x=np.array([-1.4, -0.4, 23.6]),
+                              c=np.array([[0.0323, 0.0323], [0.0210, 0.0532]]))
+    test = interpolate.PPoly(x=np.array([-0.4, 23.6]),
+                              c=np.array([[0.0323], [0.0532]]))
+    victim = utils.add_ppolyknot(test, knots=[-1.4])
+    np.testing.assert_allclose(victim.x, goal.x, atol=1e-4)
+    np.testing.assert_allclose(victim.c, goal.c, atol=1e-4)
+
+
+def test_add_ppolyknot3(ppolyspline1):
+    goal_x = np.array([-3.4, -0.4, 23.6, 28.6, 29.6])
+    goal_c = np.array([[0.0000, 0.0000, -0.0025, -0.0025],
+                       [0.0320, 0.0323, 0.0345, 0.0090],
+                       [-0.0431, 0.0532, 0.8540, 0.9628]])
+    victim = utils.add_ppolyknot(ppolyspline1, knots=[-3.4, 28.6])
+    np.testing.assert_allclose(victim.x, goal_x, atol=1e-3)
+    np.testing.assert_allclose(victim.c, goal_c, atol=1e-3)
+
+
+def test_extrapolate_spline(tck1):
+    victim = utils.extrapolate_spline(tck1, degree=1)
+    goal_c = np.array([[0, 0.0323, 0.0210],
+                       [0.0000, 0.0323, 0.0532],
+                       [-0.0025, 0.0345, 0.8540],
+                       [0, 0.0039, 0.9692]])
+    goal_x = np.array([-1.4000, -0.4000, 23.6000, 29.6000, 30.6000])
+    np.testing.assert_allclose(victim.x, goal_x, atol=1e-3)
+    np.testing.assert_allclose(victim.c, goal_c.T, atol=1e-3)
--- bayspline/utils.py
+++ bayspline/utils.py
@@ -2,10 +2,153 @@ import os
 import pkgutil
 import io
 import numpy as np
+import scipy.interpolate as interpolate
+import scipy.special as special
+
+
+def bspline2ppoly(tck, extrapolate=None):
+    """Build a piecewise polynomial from a spline, removing buffer knots
+
+    Parameters
+    ----------
+    tck
+        A spline, as returned by `splrep` or a BSpline object.
+    extrapolate : bool or 'periodic', optional
+        If bool, determines whether to extrapolate to out-of-bounds points
+        based on first and last intervals, or to return NaNs.
+        If 'periodic', periodic extrapolation is used. Default is True.
+
+    Returns
+    -------
+    ppoly: interpolate.PPoly
+        A interpolate.PPoly instance.
+
+    """
+    if isinstance(tck, interpolate.BSpline):
+        t, c, k = tck.tck
+        if extrapolate is None:
+            extrapolate = tck.extrapolate
+    else:
+        t, c, k = tck
+
+    t = t[k:-k]  # Trim buffer knots away.
+
+    cvals = np.empty((k + 1, len(t) - 1), dtype=c.dtype)
+    for m in range(k, -1, -1):
+        y = interpolate.fitpack.splev(t[:-1], tck, der=m)
+        cvals[k - m, :] = y / special.gamma(m + 1)
+
+    ppoly = interpolate.PPoly.construct_fast(cvals, t, extrapolate)
+    return ppoly
+
+
+def add_ppolyknot(ppoly, knots):
+    """Add knots to univariate piecewise polynomial
+
+    Parameters
+    ----------
+    ppoly : interpolate.PPoly
+        A univariate piecewise polynomial.
+    knots : sequence
+        Breakpoints to add to polynomial.
+
+    Returns
+    -------
+    ppoly_new : interpolate.PPoly
+        Copy of piecewise polynomial with added knots.
+    """
+    knots = np.array(knots)
+    knots.sort()
+
+    b = ppoly.x.copy()
+    k = len(ppoly.c)  # Polynomial order
+    c = ppoly.c.T.copy()
+    pieces = int(np.ceil(len(b) / 2))  # Number of pieces, I think
+
+    lb = len(knots)
+
+    index0 = np.where(knots < b[0])[0]
+    l0 = len(index0)
+
+    index2 = np.where(knots > b[pieces])[0]
+    l2 = len(index2)
+
+    index1 = np.arange(l0, (lb - l2))
+    if index1.size < 1:
+        index = index1
+        jl = np.concatenate([np.ones(l0) - 1,
+                             np.kron(np.ones(l2), pieces - 1)]).astype('int')
+    else:
+        # Find `knots` not in `b`, make their left-most point `b[link[index]]`
+        link = np.searchsorted(b[:pieces], knots[index1], side='right') - 1
+        index = np.where(b[link] != knots[index1])[0]
+        jl = np.concatenate([np.ones(l0) - 1,
+                             link[index],
+                             np.kron(np.ones(l2), pieces - 1)]).astype('int')
+    ljl = len(jl)
+
+    # Return input if all of `knots` already in `ppoly`.
+    if ljl == 0:
+        return ppoly
+
+    if l2 > 0:
+        tmp = knots[lb - 1]
+        knotsin = np.concatenate([knots[(lb - 2):(lb - l2 - 1):-1], b[[pieces]]])
+        knots[(lb - 1):(lb - l2 - 1):-1] = knotsin
+        b[pieces] = tmp
+
+    addknots = knots[np.concatenate([index0, index1[index], index2])]
+    x = addknots - b[[jl]]
+    a = c[[jl]]
+    for i in range(k - 1, 0, -1):
+        for j in range(1, i + 1):
+            a[:, j] = x * a[:, j - 1] + a[:, j]
+
+    newknots = np.concatenate([b, addknots])
+    newknots.sort()
+
+    newc = np.zeros((len(newknots) - 1, k))
+    newc[np.searchsorted(newknots, b[:pieces], side='right') - 1, :] = c
+    newc[np.searchsorted(newknots, addknots, side='right') - 1, :] = a
+
+    ppoly_new = interpolate.PPoly(c=newc.T, x=newknots)
+    return ppoly_new
+
+
+def extrapolate_spline(spline, degree=1):
+    """Extrapolate 1D function beyond its basic interval
+    """
+    ppoly = bspline2ppoly(spline)
+
+    breaks = np.array(ppoly.x)
+    coefs = np.array(ppoly.c)
+    splinedegree = len(breaks) - 1
+
+    # Add breaks outside of both ends (-1, +1)
+    newbreaks = np.array([breaks[0] - 1, breaks[-1] + 1])
+    ppoly = add_ppolyknot(ppoly, newbreaks)
+
+    splinedegree = ppoly.c.shape[0] - 1
+    if splinedegree <= degree:
+        return ppoly
+
+    newcoefs2 = np.array(ppoly.c)
+    degdif = splinedegree - degree
+
+    # In first and last polynomial, set terms greater than `degree` to 0.
+    newcoefs2[:degdif, -1:] = 0
+    newcoefs2[:degdif, 0] = 0
+
+    minipoly = interpolate.PPoly(c=newcoefs2[degdif:splinedegree + 1, 1:2],
+                                 x=ppoly.x[1:3])
+    c1 = add_ppolyknot(minipoly, ppoly.x[[0]]).c
+    newcoefs2[degdif:splinedegree + 1, 0] = c1[:, 0]
+
+    return interpolate.PPoly(c=newcoefs2, x=ppoly.x)
 
 
 def augknt(knots, degree):
-    """Augment knots to meet boundary conditiions
+    """Augment knots to meet boundary conditions
 
     Python version of MATLAB's augknt().
     """
