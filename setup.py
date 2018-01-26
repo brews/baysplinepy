20550e233decbdc09c68dbc73e1502dc32b700da
Brewster Malevich <sbmalev@gmail.com>
2018-01-26 13:22:55 -0700

    Fix bad name in setup.py.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 setup(
-    name='bayspline',
+    name='baysplinepy',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
